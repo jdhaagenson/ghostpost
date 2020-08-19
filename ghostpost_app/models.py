fd652db5f8caaa510dd020a5dcda0e3c81afb708
Jordan Haagenson <jdhaagenson@gmail.com>
2020-08-19 11:53:24 -0700

    added Post model to models

--- ghostpost_app/models.py
+++ ghostpost_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    is_boast = models.BooleanField()
+    content = models.CharField(max_length=250)
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    time_posted = models.DateTimeField()

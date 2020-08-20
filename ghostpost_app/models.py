fef9416f28d32302fc8c5d8275d8996d4e3ba83c
Jordan Haagenson <jdhaagenson@gmail.com>
2020-08-20 05:02:43 -0700

    added views for popular, homepage, form, boasts and roasts

--- ghostpost_app/models.py
+++ ghostpost_app/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
     content = models.CharField(max_length=250)
     upvotes = models.IntegerField()
     downvotes = models.IntegerField()
-    time_posted = models.DateTimeField()
+    date = models.DateTimeField()
--- ghostpost_app/views.py
+++ ghostpost_app/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from ghostpost_app.models import Post
 from ghostpost_app.forms import NewPostForm
+from datetime import datetime
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def home_view(request):
-    posts = Post.objects.all().order_by('time_posted')
+    posts = Post.objects.all().order_by('date')
+    return render(request, 'homepage.html', {'posts': posts})
+
+
+def popular_view(request):
+    posts = Post.objects.all()
+    posts.order_by('-upvotes', 'downvotes')
+    return render(request, 'homepage.html', {'posts': posts})
+
+
+def new_post_view(request):
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Post.objects.create(
+                is_boast=data.get('is_boast'),
+                content=data.get('content'),
+                date=datetime.now(),
+                upvotes=0,
+                downvotes=0
+            )
+            return HttpResponseRedirect(reverse("home_view"))
+            # return HttpResponseRedirect(reverse("secret_view"))
+    form = NewPostForm()
+    return render(request, 'newpost.html', {'form': form})
+
+
+def boasts_view(request):
+    posts = Post.objects.filter(is_boast=True)
+    return render(request, 'homepage.html', {'posts': posts})
+
+
+def roasts_view(request):
+    posts = Post.objects.filter(is_boast=False)
     return render(request, 'homepage.html', {'posts': posts})
